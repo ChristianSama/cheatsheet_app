68eddf689af4a63a0241fd09c816db0f44b8d26b
ChristianSama <christiansama1601@gmail.com>
2022-12-20 22:31:29 -0500

    add drf-writable-nested for serializer logic

--- api/models.py
+++ api/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 
 class Cheatsheet(models.Model):
-    title = models.CharField(max_length=500)
-    description = models.CharField(max_length=500)
-    score = models.IntegerField()
+    title = models.CharField(max_length=500, null=True, blank=True)
+    description = models.CharField(max_length=500, null=True, blank=True)
+    score = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 class Section(models.Model):
-    title = models.CharField(max_length=500)
-    description = models.CharField(max_length=500)
+    title = models.CharField(max_length=500, null=True, blank=True)
+    description = models.CharField(max_length=500, null=True, blank=True)
     cheatsheet = models.ForeignKey(Cheatsheet, on_delete=models.CASCADE, related_name='sections', null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 class Line(models.Model):
-    description = models.CharField(max_length=500)
-    snippet = models.CharField(max_length=500)
+    description = models.CharField(max_length=500, null=True, blank=True)
+    snippet = models.CharField(max_length=500, null=True, blank=True)
     section = models.ForeignKey(Section, on_delete=models.CASCADE, related_name='lines', null=True, blank=True)
 
     def __str__(self):
--- api/serializers.py
+++ api/serializers.py
@@ -1,79 +1,23 @@
 from rest_framework import serializers
-from .models import Cheatsheet, Section, Line
-import pdb
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+
+from api.models import Cheatsheet, Line, Section
 
 class LineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Line
         fields = "__all__"
 
-class SectionSerializer(serializers.ModelSerializer):
-    lines = LineSerializer(many=True)
+class SectionSerializer(WritableNestedModelSerializer):
+    lines = LineSerializer(many=True, required=False)
 
     class Meta:
         model = Section
         fields = "__all__"
 
-class CheatsheetSerializer(serializers.ModelSerializer):
-    sections = SectionSerializer(many=True)
+class CheatsheetSerializer(WritableNestedModelSerializer):
+    sections = SectionSerializer(many=True, required=False)
 
     class Meta:
         model = Cheatsheet
         fields = "__all__"
-
-    def create(self, validated_data):
-        sections = validated_data.pop("sections")
-        cheatsheet_instance = Cheatsheet.objects.create(**validated_data)
-        for section in sections:
-            # need to pop lines field as well: https://stackoverflow.com/questions/74724918/django-nested-serializers-direct-assignment-to-the-reverse-side-of-a-related-s
-            lines_data = section.pop("lines")
-            section_instance = Section.objects.create(cheatsheet=cheatsheet_instance, **section)
-            for line in lines_data:
-                Line.objects.create(section=section_instance, **line)
-        return cheatsheet_instance
-
-    def update(self, instance, validated_data):
-        sections = validated_data.pop("sections")
-        instance.title = validated_data.get("title")
-        instance.description = validated_data.get("description")
-        instance.score = validated_data.get("score")
-        instance.save()
-
-        instance_sections = Section.objects.filter(cheatsheet=instance.pk).values_list("id", flat=True)
-        
-        section_ids_pool = []
-
-        pdb.set_trace()
-
-        for section in sections:
-            if "id" in section.keys():
-                if Section.objects.filter(id=section['id']).exists():
-                    section_instance = Section.objects.get(id=section['id'])
-                    section_instance.title = section.get('title', section_instance.title)
-                    section_instance.description = section.get('description', section_instance.description)
-                    section_instance.save()
-
-                    #update lines
-                    #TODO: check with section without lines
-                    # for line in section['lines']:
-                    #     if "id" in line.keys():
-                    #         #update existing line
-                    #     else:
-                    #         #create new line
-                    #         new_line = Line.objects.create(section=)
-
-
-                    section_ids_pool.append(section_instance.id)
-                else: #If received Section id doesn't exist
-                    continue
-            else:
-                new_section = Section.objects.create(user=instance, **section)
-                #update lines too?
-                section_ids_pool.append(new_section.id)
-        
-        for section_id in instance_sections:
-            if section_id not in section_ids_pool:
-                Section.objects.filter(pk=section_id).delete()
-
-        return instance
-        
