8486220afade1401e23b8209a2d0acbe1817032e
ChristianSama <christiansama1601@gmail.com>
2022-12-27 00:37:25 -0500

    chore: change id for uuid in django models

--- api/models.py
+++ api/models.py
@@ -1,25 +1,29 @@
+import uuid
 from django.db import models
 
 class Cheatsheet(models.Model):
-    title = models.CharField(max_length=500, null=True, blank=True)
-    description = models.CharField(max_length=500, null=True, blank=True)
-    score = models.IntegerField(null=True, blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
+    title = models.CharField(max_length=500, null=True, blank=True, default="")
+    description = models.CharField(max_length=500, null=True, blank=True, default="")
+    score = models.IntegerField(null=True, blank=True, default=0)
 
     def __str__(self):
         return self.title
 
 class Section(models.Model):
-    title = models.CharField(max_length=500, null=True, blank=True)
-    description = models.CharField(max_length=500, null=True, blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
+    title = models.CharField(max_length=500, null=True, blank=True, default="")
+    description = models.CharField(max_length=500, null=True, blank=True, default="")
     cheatsheet = models.ForeignKey(Cheatsheet, on_delete=models.CASCADE, related_name='sections', null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 class Line(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
     description = models.CharField(max_length=500, null=True, blank=True)
-    snippet = models.CharField(max_length=500, null=True, blank=True)
-    section = models.ForeignKey(Section, on_delete=models.CASCADE, related_name='lines', null=True, blank=True)
+    snippet = models.CharField(max_length=500, null=True, blank=True, default="")
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, related_name='lines', null=True, blank=True, default="")
 
     def __str__(self):
         return self.description
