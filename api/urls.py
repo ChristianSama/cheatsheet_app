1cf29ca58f3772af1f98a2ccf1a24d69a7b07271
ChristianSama <christiansama1601@gmail.com>
2022-12-28 00:03:03 -0500

    feat: simplejwt config

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
-from api.views import CheatsheetViewSet
+from api.views import CheatsheetViewSet, MyTokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
 
 router = routers.DefaultRouter()
 router.register('cheatsheets', CheatsheetViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,22 @@
 from api.serializers import CheatsheetSerializer
 from .models import Cheatsheet
 from rest_framework import viewsets
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 class CheatsheetViewSet(viewsets.ModelViewSet):
     queryset = Cheatsheet.objects.all()
     serializer_class = CheatsheetSerializer
     http_method_names = ['get','post','retrieve','put','patch', 'delete']
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
