db03919522f8f9072e7857aec7cfb7eeb6b0d46b
ChristianSama <christiansama1601@gmail.com>
2023-01-05 23:18:45 -0500

    db setup

--- api/models.py
+++ api/models.py
@@ -1,20 +1,7 @@
-import uuid
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.translation import gettext_lazy as _
 from taggit.managers import TaggableManager
-from taggit.models import GenericUUIDTaggedItemBase, TaggedItemBase
 from vote.models import VoteModel
-
-
-class UUIDTaggedItem(GenericUUIDTaggedItemBase, TaggedItemBase):
-    # If you only inherit GenericUUIDTaggedItemBase, you need to define
-    # a tag field. e.g.
-    # tag = models.ForeignKey(Tag, related_name="uuid_tagged_items", on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = _("Tag")
-        verbose_name_plural = _("Tags")
 
 
 class Cheatsheet(VoteModel, models.Model):
@@ -29,7 +16,6 @@ class Cheatsheet(VoteModel, models.Model):
 
 
 class Section(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=500, null=True, blank=True, default="")
     description = models.CharField(
         max_length=500, null=True, blank=True, default="")
@@ -41,7 +27,6 @@ class Section(models.Model):
 
 
 class Line(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     description = models.CharField(max_length=500, null=True, blank=True)
     snippet = models.CharField(
         max_length=500, null=True, blank=True, default="")
